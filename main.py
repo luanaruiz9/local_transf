ce39f8d383f7da30638668594daf1277b87f63d5
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2022-11-18 11:40:07 -0800

    Bug fixes

--- main.py
+++ main.py
@@ -22,9 +22,15 @@ import train_test
 ""
 ""
 
-modelList = []
+# Aux class
+
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
 
 # GNN models
+
+modelList = []
 
 dataset = Planetoid(root='/tmp/cora', name='Cora')
 
@@ -40,6 +46,10 @@ modelList.append(GCN)
 # Loss
 
 loss = torch.nn.NLLLoss
+for args in [
+        {'batch_size': 32, 'hidden_dim': 32, 'dropout': 0.5, 'epochs': 500, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.01},
+    ]:
+        args = objectview(args)
 
 # Training and testing
 
@@ -47,7 +57,7 @@ for model in modelList:
     
     dataset = Planetoid(root='/tmp/cora', name='Cora')
     
-    test_accs, losses, best_model, best_acc, test_loader = train_test.train(dataset, model, loss) 
+    test_accs, losses, best_model, best_acc, test_loader = train_test.train(dataset, model, loss, args) 
 
     print("Maximum test set accuracy: {0}".format(max(test_accs)))
     print("Minimum loss: {0}".format(min(losses)))
