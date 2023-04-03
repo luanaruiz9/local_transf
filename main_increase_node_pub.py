61e0c32ba741a5c7827312c9575c5371743a4161
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-04-03 15:37:27 -0400

    Correction

--- main_increase_node_pub.py
+++ main_increase_node_pub.py
@@ -20,7 +20,7 @@ import train_test_neighbor as train_test
 
 limit_epoch = 0
 
-thisFilename = 'pubmed_node_5000' # This is the general name of all related files
+thisFilename = 'pubmed_node_3000' # This is the general name of all related files
 
 saveDirRoot = 'experiments' # In this case, relative location
 saveDir = os.path.join(saveDirRoot, thisFilename) 
@@ -42,7 +42,7 @@ class objectview(object):
         self.__dict__ = d
         
 for args in [
-        {'batch_size': 256, 'epochs': 100, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.001},
+        {'batch_size': 256, 'epochs': 150, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.001},
     ]:
         args = objectview(args)
 
@@ -56,7 +56,7 @@ n_epochs = args.epochs
 n_increases = n_epochs
 n_epochs_per_n = int(n_epochs/n_increases)
 increase_rate = 100
-n0 = 5000
+n0 = 3000
 
 for args2 in [
         {'batch_size': 256, 'epochs': n_epochs_per_n, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.001},
