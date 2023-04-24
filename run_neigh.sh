95780ccad1c03a910005a3355ab89ea799a74062
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-04-24 15:49:20 -0400

    Bug fix

--- run_neigh.sh
+++ run_neigh.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python main_increase_neigh.py cora 500 2
-python main_increase_neigh.py cora 500 5
-python main_increase_neigh.py cora 300 2
-python main_increase_neigh.py cora 300 5
-python main_increase_neigh.py citeseer 500 2
-python main_increase_neigh.py citeseer 500 5
-python main_increase_neigh.py citeseer 300 2
-python main_increase_neigh.py citeseer 300 5
-python main_increase_neigh_pub.py pubmed 2000 10
-python main_increase_neigh_pub.py pubmed 2000 15
-python main_increase_neigh_pub.py pubmed 1500 10
-python main_increase_neigh_pub.py pubmed 1500 15
+python main_increase_neighbor.py cora 500 2
+python main_increase_neighbor.py cora 500 5
+python main_increase_neighbor.py cora 300 2
+python main_increase_neighbor.py cora 300 5
+python main_increase_neighbor.py citeseer 500 2
+python main_increase_neighbor.py citeseer 500 5
+python main_increase_neighbor.py citeseer 300 2
+python main_increase_neighbor.py citeseer 300 5
+python main_increase_neighbor_pub.py pubmed 2000 10
+python main_increase_neighbor_pub.py pubmed 2000 15
+python main_increase_neighbor_pub.py pubmed 1500 10
+python main_increase_neighbor_pub.py pubmed 1500 15
 
