af182d5065549344c033fbb6c846528a76fdd323
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-08-03 16:42:57 -0400

    Bug fix

--- train_test_neighbor.py
+++ train_test_neighbor.py
@@ -109,7 +109,7 @@ def test(loader, test_model, is_validation=False, save_model_preds=False, evalua
     
     else:
         data = loader.data
-        print(data.shape)
+        print(data)
         out = test_model(data)
         y_pred = out.argmax(dim=-1, keepdim=True)
     
